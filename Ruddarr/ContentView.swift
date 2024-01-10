5a57fd24f1f3f3ee1482eda274f1dd243704aec2
Till Krüss <till@kruss.io>
2024-01-10 11:50:11 -0800

    Initial Commit

---
+++ Ruddarr/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Ruddarr
+//
+//  Created by Till Krüss on 10/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Ruddarr/RuddarrApp.swift
@@ -0,0 +1,17 @@
+//
+//  RuddarrApp.swift
+//  Ruddarr
+//
+//  Created by Till Krüss on 10/1/24.
+//
+
+import SwiftUI
+
+@main
+struct RuddarrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
