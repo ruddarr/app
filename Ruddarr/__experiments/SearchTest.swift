1f1f77912d97c22f243962d8839b647e5621bb67
Till Krüss <till@kruss.io>
2024-01-15 11:23:41 -0800

    wip

---
+++ Ruddarr/__experiments/SearchTest.swift
@@ -0,0 +1,18 @@
+//
+//  SearchTest.swift
+//  Ruddarr
+//
+//  Created by Till Krüss on 15/1/24.
+//
+
+import SwiftUI
+
+struct SearchTest: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchTest()
+}
