97a8fe92c2a7aaf5ace643a857b7737fcaad2492
Till Krüss <tillkruss@users.noreply.github.com>
2024-01-16 10:44:05 -0800

    spacing

--- Ruddarr/Views/ContentView.swift
+++ Ruddarr/Views/ContentView.swift
@@ -29,8 +29,6 @@ struct ContentView: View {
                         } else {
                             button
                         }
-
-                        
                     }
                 }
             } detail: {
