eec0911152fc03ecf2e4a67e3529f408e51cb59e
Till Krüss <till@kruss.io>
2024-01-19 21:27:34 -0800

    overhaul settings

--- Ruddarr/Views/SettingsView.swift
+++ Ruddarr/Views/SettingsView.swift
@@ -33,51 +33,128 @@ struct SettingsView: View {
         }
     }
 
+    let shareUrl = URL(string: "https://ruddarr.com")!
+    let githubUrl = URL(string: "https://github.com/tillkruss/ruddarr/")!
+    let reviewUrl = URL(string: "itms-apps://itunes.apple.com/app/id663592361")!
+
     var aboutSection: some View {
-        Section {
-            NavigationLink { ContentView() } label: {
-                Label("Help", systemImage: "questionmark.circle")
+        Section(header: Text("About")) {
+            ShareLink(item: shareUrl) {
+                Label("Share App", systemImage: "square.and.arrow.up")
+            }
+
+            Link(destination: reviewUrl, label: {
+                Label("Leave a Review", systemImage: "star")
+            })
+
+            Link(destination: supportEmailUrl(), label: {
+                Label("Email Support", systemImage: "square.and.pencil")
+            })
+
+            Link(destination: githubUrl, label: {
+                Label("Contribute on GitHub", systemImage: "chevron.left.slash.chevron.right")
+            })
+
+            NavigationLink { ThridPartyLibraries() } label: {
+                Label("Third Party Libraries", systemImage: "building.columns")
             }
         }
         .accentColor(.primary)
-        .listRowSeparatorTint(.blue)
-        .listRowSeparator(.hidden)
     }
+
+    @State private var imageCacheSize: Int = 0
+    @State private var showingEraseConfirmation: Bool = false
 
     var systemSection: some View {
         Section(header: Text("System")) {
             Button(role: .destructive, action: {
                 clearImageCache()
             }, label: {
-                LabeledContent("Clear Image Cache", value: imageCacheSize())
-            })
+                LabeledContent(
+                    "Clear Image Cache",
+                    value: ByteCountFormatter().string(fromByteCount: Int64(imageCacheSize))
+                )
+            }).onAppear {
+                calculateImageCacheSize()
+            }
 
-            Button(role: .destructive, action: {
-                if let bundleID = Bundle.main.bundleIdentifier {
-                    UserDefaults.standard.removePersistentDomain(forName: bundleID)
+            Button("Erase All Settings", role: .destructive) {
+                showingEraseConfirmation = true
+            }
+            .confirmationDialog("Are you sure?", isPresented: $showingEraseConfirmation) {
+                Button("Erase All Settings", role: .destructive) {
+                    if let bundleID = Bundle.main.bundleIdentifier {
+                        instances.removeAll()
+                        UserDefaults.standard.removePersistentDomain(forName: bundleID)
+                        showingEraseConfirmation = false
+                    }
                 }
-            }, label: {
-                Text("Erase All Settings")
-            })
+                Button("Cancel", role: .cancel) { }
+            } message: {
+                Text("Are you sure you want to erase all settings?")
+            }
         }
         .accentColor(.primary)
-        .listRowSeparatorTint(.blue)
-        .listRowSeparator(.hidden)
     }
 
-    func imageCacheSize() -> String {
-        let name = "com.github.radarr.DataCache"
-        let dataCache = try? DataCache(name: name)
-        let size = dataCache?.totalSize
-
-        return ByteCountFormatter().string(fromByteCount: Int64(size!))
+    func calculateImageCacheSize() {
+        let dataCache = try? DataCache(name: "com.github.radarr.DataCache")
+        imageCacheSize = dataCache?.totalSize ?? 0
     }
 
     func clearImageCache() {
-        let name = "com.github.radarr.DataCache"
-        let dataCache = try? DataCache(name: name)
-
+        let dataCache = try? DataCache(name: "com.github.radarr.DataCache")
         dataCache?.removeAll()
+        imageCacheSize = 0
+    }
+
+    func supportEmailUrl() -> URL {
+        let address = "support@ruddarr.com"
+        let subject = "Support Request"
+
+        let body = """
+        ---
+        The following information may help with debugging:
+
+        App Version:
+        iOS Version:
+        """
+
+        var components = URLComponents()
+        components.scheme = "mailto"
+        components.path = address
+        components.queryItems = [
+            URLQueryItem(name: "subject", value: subject),
+            URLQueryItem(name: "body", value: body)
+        ]
+
+        if let url = components.url {
+            if UIApplication.shared.canOpenURL(url) {
+                return url
+            }
+        }
+
+        return URL(string: "https://github.com/tillkruss/ruddarr/issues/")!
+    }
+}
+
+struct ThridPartyLibraries: View {
+    var body: some View {
+        List {
+            Link(destination: URL(string: "https://github.com/kean/Nuke")!, label: {
+                HStack {
+                    Text("Nuke")
+                    Text("12.3.0").foregroundColor(.secondary)
+                    Spacer()
+                    Image(systemName: "chevron.right")
+                        .imageScale(.small)
+                        .foregroundColor(.secondary)
+                }
+            })
+        }
+        .accentColor(.primary)
+        .navigationTitle("Third Party Libraries")
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
