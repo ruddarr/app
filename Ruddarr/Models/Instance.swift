2a621de6c56c86e69e4a9ed45bf39183e9b25b00
Petr Sima <petrsima@icloud.com>
2024-01-25 11:02:54 +0100

    shuffled code around

--- Ruddarr/Models/Instance.swift
+++ Ruddarr/Models/Instance.swift
@@ -17,39 +17,6 @@ enum InstanceType: String, Identifiable, CaseIterable, Codable {
 struct InstanceStatus: Codable {
   let appName: String
 }
-
-extension Array<Instance>: RawRepresentable {
-    public init?(rawValue: String) {
-        guard let data = rawValue.data(using: .utf8),
-            let result = try? JSONDecoder().decode([Instance].self, from: data)
-        else {
-            return nil
-        }
-        self = result
-    }
-
-    public var rawValue: String {
-        guard let data = try? JSONEncoder().encode(self),
-            let result = String(data: data, encoding: .utf8)
-        else {
-            return "[]"
-        }
-        return result
-    }
-}
-
-extension UUID: RawRepresentable {
-    public var rawValue: String {
-        self.uuidString
-    }
-
-    public typealias RawValue = String
-
-    public init?(rawValue: RawValue) {
-        self.init(uuidString: rawValue)
-    }
-}
-
 extension Instance {
     static var sample: Self {
         .init(
--- Ruddarr/Views/MoviesView.swift
+++ Ruddarr/Views/MoviesView.swift
@@ -280,58 +280,6 @@ struct MovieRow: View {
     }
 }
 
-struct MovieSort: Codable {
-    var isAscending: Bool = true
-    var option: Option = .byTitle
-
-    enum Option: CaseIterable, Hashable, Identifiable, Codable {
-        var id: Self { self }
-        case byTitle
-        case byYear
-        case byAdded
-
-        var title: String {
-            switch self {
-            case .byTitle: "Title"
-            case .byYear: "Year"
-            case .byAdded: "Added"
-            }
-        }
-
-        func isOrderedBefore(_ lhs: Movie, _ rhs: Movie) -> Bool {
-            switch self {
-            case .byTitle:
-                lhs.sortTitle < rhs.sortTitle
-            case .byYear:
-                lhs.year < rhs.year
-            case .byAdded:
-                lhs.added < rhs.added
-            }
-        }
-    }
-}
-
-extension MovieSort: RawRepresentable {
-    public init?(rawValue: String) {
-        guard let data = rawValue.data(using: .utf8),
-            let result = try? JSONDecoder().decode(MovieSort.self, from: data)
-        else {
-            return nil
-        }
-        self = result
-    }
-
-    public var rawValue: String {
-        guard let data = try? JSONEncoder().encode(self),
-            let result = String(data: data, encoding: .utf8)
-        else {
-            return "[]"
-        }
-        return result
-    }
-}
-
-
 #Preview {
     ContentView()
 }
