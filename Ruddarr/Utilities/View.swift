ce17e054cea35178eb75ae8b36149c6c9ec064c2
Till Krüss <till@kruss.io>
2024-01-29 15:18:01 -0800

    use view mondifiers

--- Ruddarr/Utilities/View.swift
+++ Ruddarr/Utilities/View.swift
@@ -1,22 +1,13 @@
 import SwiftUI
 
 extension View {
-    func withAppState() -> some View {
-        let settings = AppSettings()
-        let radarrInstance = settings.radarrInstance ?? Instance.void
 
-        return self
-            .tint(settings.accentColor())
-            .environmentObject(settings)
-            .environment(RadarrInstance(radarrInstance))
+    func withAppState() -> some View {
+        modifier(WithAppStateModifier())
     }
 
     func withSettings() -> some View {
-        let settings = AppSettings()
-
-        return self
-            .tint(settings.accentColor())
-            .environmentObject(AppSettings())
+        modifier(WithSettingsModifier())
     }
 
     func withRadarrInstance(movies: [Movie] = [], lookup: [Movie] = []) -> some View {
@@ -24,5 +15,31 @@ extension View {
         instance.movies.items = movies
 
         return self.environment(instance)
+    }
+}
+
+private struct WithAppStateModifier: ViewModifier {
+    @AppStorage("theme", store: dependencies.store) var theme: Theme = .factory
+
+    func body(content: Content) -> some View {
+        let settings = AppSettings()
+        let radarrInstance = settings.radarrInstance ?? Instance.void
+
+        content
+            .tint(theme.tint)
+            .environmentObject(settings)
+            .environment(RadarrInstance(radarrInstance))
+    }
+}
+
+private struct WithSettingsModifier: ViewModifier {
+    @AppStorage("theme", store: dependencies.store) var theme: Theme = .factory
+
+    func body(content: Content) -> some View {
+        let settings = AppSettings()
+
+        content
+            .tint(theme.tint)
+            .environmentObject(settings)
     }
 }
