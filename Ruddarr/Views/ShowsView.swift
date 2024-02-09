6a53c946ee1b104ce460b2e70f8d1a76a7c18dbc
Till Krüss <till@kruss.io>
2024-02-08 18:01:08 -0800

    add tv shows notice; closes #89

--- Ruddarr/Views/ShowsView.swift
+++ Ruddarr/Views/ShowsView.swift
@@ -2,7 +2,11 @@ import SwiftUI
 
 struct ShowsView: View {
     var body: some View {
-        Text("TV Shows")
+        ContentUnavailableView(
+            "Coming Soon",
+            systemImage: "tv.slash",
+            description: Text("TV Shows will be added in a few weeks, once the Movies module has been polished and thoroughly tested in TestFlight.")
+        )
     }
 }
 
