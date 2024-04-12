60d7bd6537a4418a186fe021367cde1a76ccdf34
Till Krüss <till@kruss.io>
2024-04-12 09:15:03 -0700

    tweak history date display

--- Ruddarr/Views/Movies/MovieMetadataView.swift
+++ Ruddarr/Views/Movies/MovieMetadataView.swift
@@ -198,7 +198,15 @@ struct MovieHistoryItem: View {
         let twoWeeksAgo = Calendar.current.date(byAdding: .day, value: -14, to: Date())!
 
         if event.date > twoWeeksAgo {
-            return event.date.formatted(.relative(presentation: .named, unitsStyle: .narrow))
+            let formatter = RelativeDateTimeFormatter()
+            formatter.dateTimeStyle = .numeric
+            formatter.unitsStyle = .abbreviated
+
+            return formatter.localizedString(for: event.date, relativeTo: Date())
+        }
+
+        if Calendar.current.isDate(event.date, equalTo: .now, toGranularity: .year) {
+            return event.date.formatted(.dateTime.day().month())
         }
 
         return event.date.formatted(date: .abbreviated, time: .omitted)
