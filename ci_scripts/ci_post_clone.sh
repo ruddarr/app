a6dae9f2999c305cb0738a86ca76fc169288f475
Till Krüss <till@kruss.io>
2024-04-17 09:06:40 -0700

    wrap in quotes

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -4,5 +4,5 @@ set -e
 
 cd $CI_WORKSPACE/Ruddarr || exit 1
 
-plutil -replace APNsKey -string $APNS_SECRET Info.plist
+plutil -replace APNsKey -string "$APNS_SECRET" Info.plist
 plutil -p Info.plist
