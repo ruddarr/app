674b47c6a6096936c7934fd3d51d44c8b3ba089f
Till Krüss <till@kruss.io>
2024-04-17 09:32:17 -0700

    finalize `ci_post_clone.sh` script

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -2,20 +2,6 @@
 
 set -e
 
-# for future reference
-# https://developer.apple.com/documentation/xcode/environment-variable-reference
-
-echo $(pwd)
-echo $CI_XCODE_PROJECT
-echo $CI_WORKSPACE_PATH
-echo $CI_PROJECT_FILE_PATH
-echo $CI_PRIMARY_REPOSITORY_PATH
-echo $CI_DERIVED_DATA_PATH
-
-cd ../Ruddarr/
-# cd $CI_WORKSPACE/Ruddarr || exit 1
-
-echo $(pwd)
+cd ../Ruddarr/ || exit 1
 
 plutil -replace APNsKey -string "$APNS_SECRET" Info.plist
-plutil -p Info.plist
