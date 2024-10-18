1d8563da0c1ec59f29e89c921307725640a3a1b5
Pavlo Yatsukhnenko <yatsukhnenko@gmail.com>
2024-10-18 21:22:03 +0300

    Show SeriesMonitoredTip depending on series.monitored value

--- Ruddarr/Views/Shared/Tips.swift
+++ Ruddarr/Views/Shared/Tips.swift
@@ -1,12 +1,26 @@
 import TipKit
 
 struct SeriesMonitoringTip: Tip {
+    @Parameter static var seriesMonitored: Bool = true
+
     var title: Text {
         Text("Series Not Monitored")
     }
 
     var message: Text? {
         Text("The series itself must be monitored, in order to monitor a season or episode.")
+    }
+
+    var rules: [Rule] {
+        [
+            #Rule(Self.$seriesMonitored) {
+                $0 == false
+            }
+        ]
+    }
+
+    init(_ seriesMonitored: Bool) {
+        Self.seriesMonitored = seriesMonitored
     }
 }
 
