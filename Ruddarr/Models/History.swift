ab94ac35c01ca1c9131a902848a22c8d93471716
Till Krüss <till@kruss.io>
2024-11-05 09:30:46 -0800

    fetch history data in parallel

--- Ruddarr/Models/History.swift
+++ Ruddarr/Models/History.swift
@@ -24,25 +24,31 @@ class History {
 
         var results: [MediaHistoryEvent] = []
 
-        for instance in instances {
-            if !(hasMore[instance.id] ?? true) {
-                continue
+        do {
+            try await withThrowingTaskGroup(of: Void.self) { group in
+                for instance in instances {
+                    if !(hasMore[instance.id] ?? true) {
+                        continue
+                    }
+
+                    group.addTask {
+                        let result = try await dependencies.api.fetchHistory(page, 25, instance)
+
+                        results.append(contentsOf: result.records)
+                        self.hasMore[instance.id] = result.totalRecords > result.page * result.pageSize
+                    }
+                }
+
+                try await group.waitForAll()
             }
+        } catch is CancellationError {
+            // do nothing
+        } catch let apiError as API.Error {
+            error = apiError
 
-            do {
-                let result = try await dependencies.api.fetchHistory(page, 25, instance)
-
-                results.append(contentsOf: result.records)
-                hasMore[instance.id] = result.totalRecords > result.page * result.pageSize
-            } catch is CancellationError {
-                // do nothing
-            } catch let apiError as API.Error {
-                error = apiError
-
-                leaveBreadcrumb(.error, category: "history", message: "History fetch failed", data: ["error": apiError])
-            } catch {
-                self.error = API.Error(from: error)
-            }
+            leaveBreadcrumb(.error, category: "history", message: "History fetch failed", data: ["error": apiError])
+        } catch {
+            self.error = API.Error(from: error)
         }
 
         events.append(contentsOf: results)
