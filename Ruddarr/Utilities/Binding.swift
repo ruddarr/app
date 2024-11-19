4c4cfad0e305c59dabbc7f779e999c7669db4d40
Till Krüss <till@kruss.io>
2024-11-19 11:46:13 -0800

    remove unused binding extensions

--- Ruddarr/Utilities/Binding.swift
+++ Ruddarr/Utilities/Binding.swift
@@ -1,28 +1,5 @@
 import SwiftUI
 
-extension Binding {
-    func onSet(_ action: @escaping (Value) -> Void = { _ in }) -> Self {
-        .init {
-            wrappedValue
-        } set: {
-            action($0)
-            wrappedValue = $0
-        }
-    }
-}
-
-extension Binding {
-    var optional: Binding<Value?> {
-        .init {
-            wrappedValue
-        } set: {
-            if let value = $0 {
-                wrappedValue = value
-            }
-        }
-    }
-}
-
 extension Binding where Value: OptionalProtocol {
     var unwrapped: Binding<Value.Wrapped>? {
         guard var wrappedValue = self.wrappedValue.wrappedValue else {
