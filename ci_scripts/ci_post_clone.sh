d28263cbadcaf39983d7129b8cf249587356c035
Till Krüss <till@kruss.io>
2025-03-22 16:28:38 -0700

    wip

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -2,6 +2,27 @@
 
 set -e
 
-brew install yq jq
+cd .. || exit 1
 
-yq '.project_id' crowdin.yml
+brew install crowdin yq
+
+EXPORTED=(en $(yq '.export_languages[]' crowdin.yml))
+
+# Convert to Apple language codes
+EXPORTED=("${EXPORTED[@]/es-ES/es}")
+
+# Download translations
+FLAGS=$(yq '.export_languages[]' crowdin.yml | awk '{printf "--language=%s ", $0}')
+crowdin download translations --plain ${FLAGS}
+
+cd Ruddarr
+
+# Remove languages that are not exported from *.xcstrings catalogs
+for file in Localizable.xcstrings AppShortcuts.xcstrings; do
+  for lang in $(jq -r '.strings[].localizations | keys[]' $file | sort -u); do
+    if [[ ! ${EXPORTED[@]} =~ $lang ]]; then
+      jq "del(.strings[].localizations.${lang})" $file > "${file}.json"
+      mv "${file}.json" $file
+    fi
+  done
+done
