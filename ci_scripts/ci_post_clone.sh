3d82222d62d424326b5ddf68948b30312d3e88dd
Till Krüss <till@kruss.io>
2025-03-22 12:26:19 -0700

    use brew?

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -2,9 +2,6 @@
 
 set -e
 
-echo $PATH
-
-curl https://github.com/mikefarah/yq/releases/latest/download/yq_darwin_arm64 -O /usr/bin/yq \
-  && chmod +x /usr/bin/yq
+brew install yq jq
 
 yq '.project_id' crowdin.yml
