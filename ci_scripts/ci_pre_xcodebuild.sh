22c150e1cd952f46950c6d278bc49f3489e141e4
Till Krüss <till@kruss.io>
2025-09-19 19:28:06 -0700

    fix typo?!

--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -3,7 +3,7 @@
 set -e
 
 # SwiftLint
-defaults write com.apple.dt.Xcode IDESkipPackagePluginFingerprintValidatation -bool YES
+defaults write com.apple.dt.Xcode IDESkipPackagePluginFingerprintValidation -bool YES
 defaults write com.apple.dt.Xcode IDESkipMacroFingerprintValidation -bool YES
 
 cd ../Ruddarr/ || exit 1
