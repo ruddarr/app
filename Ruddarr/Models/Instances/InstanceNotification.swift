00509937374370fc0e7b8c0be2b0a50ae2d106c0
Lukas McDiarmid <lukasmcd14@users.noreply.github.com>
2026-05-21 14:57:34 +1000

    Notification support for Lidarr

--- Ruddarr/Models/Instances/InstanceNotification.swift
+++ Ruddarr/Models/Instances/InstanceNotification.swift
@@ -27,6 +27,11 @@ struct InstanceNotification: Identifiable, Codable {
     var onEpisodeFileDelete: Bool? = false
     var onImportComplete: Bool? = false
 
+    // Lidarr only
+    var onArtistAdd: Bool? = false
+    var onArtistDelete: Bool? = false
+    var onAlbumDelete: Bool? = false
+
     var onManualInteractionRequired: Bool? = false
 
     var onHealthIssue: Bool = false
@@ -57,12 +62,19 @@ struct InstanceNotification: Identifiable, Codable {
     var supportsOnImportComplete: Bool? = false
     var supportsOnManualInteractionRequired: Bool? = false
 
+    // Lidarr
+    var supportsOnArtistAdd: Bool? = false
+    var supportsOnArtistDelete: Bool? = false
+    var supportsOnAlbumDelete: Bool? = false
+    var supportsOnAlbumDeleteForUpgrade: Bool? = false
+
     var isEnabled: Bool {
         onGrab
         || onDownload
         || onUpgrade
         || onMovieAdded ?? false
         || onSeriesAdd ?? false
+        || onArtistAdd ?? false
         || onImportComplete ?? false
         || onHealthIssue
         || onHealthRestored ?? false
@@ -77,6 +89,7 @@ struct InstanceNotification: Identifiable, Codable {
         onMovieAdded = false // Radarr
         onSeriesAdd = false // Sonarr
         onImportComplete = false // Sonarr
+        onArtistAdd = false  // Lidarr
         onHealthIssue = false
         onHealthRestored = false
         includeHealthWarnings = false
@@ -91,6 +104,7 @@ struct InstanceNotification: Identifiable, Codable {
         onManualInteractionRequired = supportsOnManualInteractionRequired == true
         onMovieAdded = supportsOnMovieAdded == true // Radarr
         onSeriesAdd = supportsOnSeriesAdd == true // Sonarr
+        onArtistAdd = supportsOnArtistAdd == true // Lidarr
         onImportComplete = false // Sonarr
         onHealthIssue = false
         onHealthRestored = false
