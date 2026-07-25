2e5ec8968ecfbcd53c21856a177b29bcc04ee26d
Till Krüss <till@kruss.io>
2026-07-24 19:29:17 -0700

    move

--- Ruddarr/Services/Sentry.swift
+++ Ruddarr/Services/Sentry.swift
@@ -186,37 +186,3 @@ private func maskedBreadcrumbData(_ data: [String: Any]) -> [String: Any] {
         return value
     }
 }
-
-enum EnvironmentType: String {
-    case preview
-    case simulator
-    case debug
-    case testflight
-    case appstore
-
-    static let cached: EnvironmentType = {
-        guard let branch = Bundle.main.object(forInfoDictionaryKey: "CI_BRANCH") as? String else {
-            return .appstore
-        }
-
-        return branch.contains("develop") ? .testflight : .appstore
-    }()
-}
-
-func isRunningIn(_ env: EnvironmentType) -> Bool {
-    runningIn() == env
-}
-
-func runningIn() -> EnvironmentType {
-    if ProcessInfo.processInfo.environment["XCODE_RUNNING_FOR_PREVIEWS"] == "1" {
-        return .preview
-    }
-
-#if targetEnvironment(simulator)
-    return .simulator
-#elseif DEBUG
-    return .debug
-#else
-    return EnvironmentType.cached
-#endif
-}
