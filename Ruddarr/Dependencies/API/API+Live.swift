1c9ae12b1fc7d927fa5b189af5444c27fe8b2800
Till Krüss <till@kruss.io>
2026-08-09 16:41:38 -0700

    just fetch more

--- Ruddarr/Dependencies/API/API+Live.swift
+++ Ruddarr/Dependencies/API/API+Live.swift
@@ -258,25 +258,10 @@ extension API {
                     .init(name: "includeMovie", value: "true"),
                     .init(name: "includeSeries", value: "true"),
                     .init(name: "includeEpisode", value: "true"),
-                    .init(name: "pageSize", value: "100"),
+                    .init(name: "pageSize", value: "250"),
                 ])
 
             var items: QueueItems = try await request(url: url, instance: instance)
-            var page = 1
-
-            while items.records.count < items.totalRecords, page < 5 {
-                page += 1
-
-                let next: QueueItems = try await request(
-                    url: url.appending(queryItems: [.init(name: "page", value: String(page))]),
-                    instance: instance
-                )
-
-                guard !next.records.isEmpty else { break }
-
-                items.records += next.records
-            }
-
             items.records.stamp(instance.id)
             return items
         }, deleteQueueTask: { task, remove, block, search, instance in
